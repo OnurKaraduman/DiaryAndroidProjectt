f7c9dc73a4ccb3f66322010cc0f0d4561bd4211c
Onur <onrkrdmn@gmail.com>
2014-11-26 01:11:24 +0200

    constants continuee

--- DiaryAndroidProject2/src/com/iuce/constants/DBConstants.java
+++ DiaryAndroidProject2/src/com/iuce/constants/DBConstants.java
@@ -2,4 +2,26 @@ package com.iuce.constants;
 
 public class DBConstants {
 
+	public static String PACKAGE_NAME = "com.iuce.diaryandroidproject2";
+	public static String PROJECT_TAG = "DiaryProject";
+
+	// genel bilgiler
+	public static String DATABASE_NAME = "Diary.db";
+	public static int DATABASE_VERSION = 1;
+	public static String DIARY_TABLE = "diary";
+
+	public static String DB_PATH = "content://" + PACKAGE_NAME + "/diary";
+	
+	//diary tablosu kolon isimleri
+	public static String ID = "id";
+	public static String DATE = "date";
+	public static String TITLE = "title";
+	public static String CONTENT = "content";
+	public static String LONGITUDE = "longitude";
+	public static String LATITUDE = "latitude";
+	
+	
+	
+	
+	
 }
